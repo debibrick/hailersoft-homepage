572a939ee040bfdd5b61609a15936a927e443431
velbie <velbie13@gmail.com>
2021-11-23 16:23:09 +0900

    .

---
+++ src/components/Team.jsx
@@ -0,0 +1,31 @@
+export const Team = (props) => {
+  return (
+    <div id='team' className='text-center'>
+      <div className='container'>
+        <div className='col-md-8 col-md-offset-2 section-title'>
+          <h2>Meet the Team</h2>
+          <p>
+            최고의 스페셜리스트 
+능동적이고 책임감 있는 전문가들이 함께 성장하며, 우리 주변의 문제를 해결합니다.
+          </p>
+        </div>
+        <div id='row'>
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className='col-md-3 col-sm-6 team'>
+                  <div className='thumbnail'>
+                    {' '}
+                    <img src={d.img} alt='...' className='team-img' />
+                    <div className='caption'>
+                      <h4>{d.name}</h4>
+                      <p>{d.job}</p>
+                    </div>
+                  </div>
+                </div>
+              ))
+            : 'loading'}
+        </div>
+      </div>
+    </div>
+  )
+}
